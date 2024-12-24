f072fa73c432b8c627d24a603123e2e00ca794c3
yu.nezhelskaya <yu.nezhelskaya@qpdev.ru>
2024-12-24 13:45:38 +0300

    ревалидация по требованию

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.query.secret !== process.env.NEXT_PUBLIC_SECRET_TOKEN) {
+    return res.status(401).json({ message: "ok" });
+  }
+
+  try {
+    await res.revalidate("/test");
+    return res.json({ revalidate: true });
+  } catch (err) {
+    return res.status(500).send("Error");
+  }
+}
+
+// что бы данные изменить по требованию необходимо проходить вот по жтому эндпоинту
